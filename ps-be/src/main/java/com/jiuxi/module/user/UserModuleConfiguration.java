b52e91be03c3b1bc9e11b704d285f7de493253b6
jiuxi-tech <jiuxi-tech@github.com>
2025-09-14 02:27:26 +0800

    feat(用户模块): 添加用户模块配置类并更新服务层结构
    添加UserModuleConfiguration类用于配置用户模块的组件扫描和事务管理
    更新服务层结构文档，增加应用服务实现目录说明

---
+++ ps-be/src/main/java/com/jiuxi/module/user/UserModuleConfiguration.java
@@ -0,0 +1,29 @@
+package com.jiuxi.module.user;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+/**
+ * 用户模块配置类
+ * 
+ * 定义用户模块的组件扫描范围和配置
+ * 
+ * @author 系统重构
+ * @since 2.2.2
+ */
+@Configuration
+@ComponentScan({
+    "com.jiuxi.module.user.app",
+    "com.jiuxi.module.user.domain",
+    "com.jiuxi.module.user.infra",
+    "com.jiuxi.module.user.interfaces"
+})
+@MapperScan("com.jiuxi.module.user.infra.persistence.mapper")
+@EnableTransactionManagement
+public class UserModuleConfiguration {
+    
+    // 用户模块的特定配置可以在这里添加
+    
+}
