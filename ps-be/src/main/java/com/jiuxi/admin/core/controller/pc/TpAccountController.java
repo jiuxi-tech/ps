ea842dd2bb77c93c72941d9d38ace09033884eb2
jiuxi-tech <jiuxi-tech@github.com>
2025-09-19 00:54:45 +0800

    feat(账号管理): 添加账号控制器及修改密码功能
    新增TpAccountController控制器，实现账号管理功能
    添加修改密码接口，包含参数校验和密码更新逻辑

---
+++ ps-be/src/main/java/com/jiuxi/admin/core/controller/pc/TpAccountController.java
@@ -0,0 +1,47 @@
+package com.jiuxi.admin.core.controller.pc;
+
+import cn.hutool.core.util.StrUtil;
+import com.jiuxi.module.user.app.service.UserAccountService;
+import com.jiuxi.admin.core.service.TpAccountService;
+import com.jiuxi.common.bean.JsonResponse;
+import com.jiuxi.shared.common.annotation.IgnoreAuthorization;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Description: 账号表
+ * @ClassName: TpAccountController
+ * @Author: pand
+ * @Date: 2021-06-17 15:34
+ * @Copyright: 2021 www.tuxun.net Inc. All rights reserved.
+ */
+@RestController
+@RequestMapping("/sys/account")
+public class TpAccountController {
+
+    @Autowired
+    private TpAccountService tpAccountService;
+    
+    @Autowired
+    private UserAccountService userAccountService;
+
+    /**
+     * 修改密码
+     */
+    @RequestMapping("/update")
+    @IgnoreAuthorization
+    public JsonResponse update(String userpwd, String jwtpid) {
+        if (StrUtil.isBlank(userpwd)) {
+            return JsonResponse.buildFailure("请输入密码！");
+        }
+        
+        if (StrUtil.isBlank(jwtpid)) {
+            return JsonResponse.buildFailure("用户身份信息缺失！");
+        }
+
+        userAccountService.updatePwd(jwtpid, userpwd);
+        return JsonResponse.buildSuccess();
+    }
+
+}
